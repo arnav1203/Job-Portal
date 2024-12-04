7abed3d6de310f139538282aa27eae8d5bceb3a8
arnav1203 <2101641530040@psit.ac.in>
2024-12-05 02:32:11 +0530

    Backend work

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+const app = express;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+const PORT = 8000;
+app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`);
+
+})
