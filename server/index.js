7b4819073cc35b8ea7a1aef966d23db6613d439e
arnav1203 <2101641530040@psit.ac.in>
2024-12-05 03:02:03 +0530

    Update

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 const app = express;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+const corsOptions = {
+    origin: 'http//localhost:3000',
+    credentials: true
+}
+app.use(cors(corsOptions));
 
 const PORT = 8000;
 app.listen(PORT, () => {
