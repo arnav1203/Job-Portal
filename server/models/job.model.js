0bcf88599f59488e0c7c99b2a7bf1b2415454504
arnav1203 <2101641530040@psit.ac.in>
2024-12-05 15:36:23 +0530

    Job model schema making

---
+++ server/models/job.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const jobSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    requirements: [{
+        type: String,
+    }],
+    salary: {
+        type: Number,
+        required: true
+    }
+})
