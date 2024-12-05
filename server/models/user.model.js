3fbd96830b34aefdafdb97c3bbc06ac311dc82b9
arnav1203 <2101641530040@psit.ac.in>
2024-12-05 15:32:34 +0530

    Schema done

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -28,7 +28,15 @@ const userSchema = new mongoose.Schema({
             type: String,
         },
         skills: [{ type: String }],
-        resume: { type: String }
+        resume: { type: String },
+        resumeOriginalName: { type: String },
+        company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company' },
+        profilePhoto: {
+            type: String,
+            default: ""
+        }
     }
 
-})
+}, { timestamps: true });
+
+export const User = mongoose.model('User', userSchema);
