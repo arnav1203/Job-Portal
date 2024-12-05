099754daec591e9bb15b9f0b11747446145004fa
arnav1203 <2101641530040@psit.ac.in>
2024-12-05 15:18:17 +0530

    Update usermodel

---
+++ server/models/user.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phonenumber: {
+        type: Number,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+
+    }
+})
