21e58e68665da4a4678371baa293cbe3c6387d86
arnav1203 <2101641530040@psit.ac.in>
2024-12-12 00:48:15 +0530

    applyjob update

--- server/controllers/application.controller.js
+++ server/controllers/application.controller.js
@@ -1,3 +1,6 @@
+import { Application } from "../models/application.model";
+import { Job } from "../models/job.model";
+
 export const applyJob = async (req, res) => {
     try {
         const userId = req.id;
@@ -9,6 +12,33 @@ export const applyJob = async (req, res) => {
             })
         }
         // check is user has already applied for job
+        const existingApplication = await Application.findOne({ job: jobId, applicant: userId });
+
+        if (existingApplication) {
+            return res.status(400).json({
+                message: "You have already applied for this job.",
+                success: false
+            })
+        }
+        // check if job exists
+        const job = await Job.findById(jobId);
+        if (!job) {
+            return res.status(400).json({
+                message: "Job not found",
+                success: false
+            })
+        }
+        //create new application
+        const newApplication = await Application.create({
+            job: jobId,
+            applicant: userId
+        })
+        job.applications.push(newApplication._id);
+        await job.save();
+        return res.status(201).json({
+            message: "Job applied successfully",
+            success: true
+        })
     } catch (error) {
         console.log(error);
 
