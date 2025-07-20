d4af4d326878880df5a155495ba00157cd1ccc84
arnav1203 <2101641530040@psit.ac.in>
2025-07-20 21:38:59 +0530

    routing start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
-import Navbar from "./components/shared/Navbar"
-
+import { createBrowserRouter } from "react-router-dom";
+import Navbar from "./components/shared/Navbar";
+const approuter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/",
+    element: <Signup />,
+  },
+]);
 function App() {
   return (
     <>
       <Navbar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/shared/Navbar.jsx
+++ client/src/components/shared/Navbar.jsx
@@ -24,43 +24,52 @@ const Navbar = () => {
             <li>Jobs</li>
             <li>Browse</li>
           </ul>
-          <Popover>
-            <PopoverTrigger asChild>
-              <Avatar className="cursor-pointer">
-                <AvatarImage
-                  src="https://github.com/shadcn.png"
-                  alt="@shadcn"
-                />
-              </Avatar>
-            </PopoverTrigger>
-            <PopoverContent className="w-80">
-              <div className="flex gap-4 space-y-2">
+          {!user ? (
+            <div className="flex items-center gap-2">
+              <Button variant="outline">Login</Button>
+              <Button className="bg-[#6A38C2] hover:bg-[#5b30a6]">
+                Signup
+              </Button>
+            </div>
+          ) : (
+            <Popover>
+              <PopoverTrigger asChild>
                 <Avatar className="cursor-pointer">
                   <AvatarImage
                     src="https://github.com/shadcn.png"
                     alt="@shadcn"
                   />
                 </Avatar>
-                <div>
-                  <h4 className="font-medium">Arnav Mernstack</h4>
-                  <p className="text-sm text-muted-foreground">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    aspernatur?
-                  </p>
+              </PopoverTrigger>
+              <PopoverContent className="w-80">
+                <div className="flex gap-4 space-y-2">
+                  <Avatar className="cursor-pointer">
+                    <AvatarImage
+                      src="https://github.com/shadcn.png"
+                      alt="@shadcn"
+                    />
+                  </Avatar>
+                  <div>
+                    <h4 className="font-medium">Arnav Mernstack</h4>
+                    <p className="text-sm text-muted-foreground">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      aspernatur?
+                    </p>
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-col my-2 text-gray-600">
-                <div className="flex w-fit items-center gap-2 cursor-pointer">
-                  <User2 />
-                  <Button variant="link">View Profile</Button>
+                <div className="flex flex-col my-2 text-gray-600">
+                  <div className="flex w-fit items-center gap-2 cursor-pointer">
+                    <User2 />
+                    <Button variant="link">View Profile</Button>
+                  </div>
+                  <div className="flex w-fit items-center gap-2 cursor-pointer">
+                    <LogOut />
+                    <Button variant="link">Logout</Button>
+                  </div>
                 </div>
-                <div className="flex w-fit items-center gap-2 cursor-pointer">
-                  <LogOut />
-                  <Button variant="link">Logout</Button>
-                </div>
-              </div>
-            </PopoverContent>
-          </Popover>
+              </PopoverContent>
+            </Popover>
+          )}
         </div>
       </div>
     </div>
