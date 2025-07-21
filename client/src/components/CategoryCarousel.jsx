37e544f10683f67caa96ebab12a916c68ab159a4
arnav1203 <2101641530040@psit.ac.in>
2025-07-22 01:41:56 +0530

    Category Caorusel work

--- client/src/components/CategoryCarousel.jsx
+++ client/src/components/CategoryCarousel.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+const category = [
+  "Frontend Developer",
+  "Backend Developer",
+  "Data Science",
+  "Graphic Designer",
+  "Full Stack Developer",
+];
+
 const CategoryCarousel = () => {
   return <div>Category Carousel Component</div>;
 };
