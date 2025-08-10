a2b1e3d1957af49a210d9d7b079fb22e874dfe23
arnav1203 <2101641530040@psit.ac.in>
2025-08-10 14:01:00 +0530

    Apply part

--- client/src/components/JobDescription.jsx
+++ client/src/components/JobDescription.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { Badge } from "./ui/badge";
+import { Button } from "./ui/button";
 
 const JobDescription = () => {
-  return <div>JobDescription</div>;
+  const isApplied = true;
+  return (
+    <div className="max-w-7xl mx-auto my-10">
+      <div className="flex items-center justify-between">
+        <div>
+          <h1 className="font-bold text-xl">Title</h1>
+          <div className="flex items-center gap-2 mt-4">
+            <Badge className={"text-blue-700 font-bold"} variant={"ghost"}>
+              12 Positions
+            </Badge>
+            <Badge className={"text-[#F83002] font-bold"} variant={"ghost"}>
+              Part Time
+            </Badge>
+            <Badge className={"text-[#7209b7] font-bold"} variant={"ghost"}>
+              24Lpa
+            </Badge>
+          </div>
+        </div>
+        <Button>{isApplied ? "Already Applied" : "Apply Now"}</Button>
+      </div>
+    </div>
+  );
 };
 
 export default JobDescription;
