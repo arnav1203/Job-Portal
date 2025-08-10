706067ed3da679419c397b1a51115d6f06e5432c
arnav1203 <2101641530040@psit.ac.in>
2025-08-10 19:43:22 +0530

    Job description dynamic

--- client/src/components/JobDescription.jsx
+++ client/src/components/JobDescription.jsx
@@ -1,23 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Badge } from "./ui/badge";
 import { Button } from "./ui/button";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { JOB_API_END_POINT } from "@/utils/constant";
+import { setSingleJob } from "@/redux/jobSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const JobDescription = () => {
   const isApplied = true;
+  const params = useParams();
+  const jobId = params.id;
+  const { singleJob } = useSelector((store) => store.job);
+  const { user } = useSelector((store) => store.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchSingleJob = async () => {
+      try {
+        const res = await axios.get(`${JOB_API_END_POINT}/get/${jobId}`, {
+          withCredentials: true,
+        });
+        if (res.data.success) {
+          dispatch(setSingleJob(res.data.job));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchSingleJob();
+  }, [jobId, dispatch, user?._id]);
   return (
     <div className="max-w-7xl mx-auto my-10">
       <div className="flex items-center justify-between">
         <div>
-          <h1 className="font-bold text-xl">FrontEnd Developer</h1>
+          <h1 className="font-bold text-xl">{singleJob?.title}</h1>
           <div className="flex items-center gap-2 mt-4">
             <Badge className={"text-blue-700 font-bold"} variant={"ghost"}>
-              12 Positions
+              {singleJob?.position} Positions
             </Badge>
             <Badge className={"text-[#F83002] font-bold"} variant={"ghost"}>
-              Part Time
+              {singleJob?.jobType}
             </Badge>
             <Badge className={"text-[#7209b7] font-bold"} variant={"ghost"}>
-              24Lpa
+              {singleJob?.salary}Lpa
             </Badge>
           </div>
         </div>
@@ -39,31 +65,38 @@ const JobDescription = () => {
         <h1 className="font-bold my-1">
           Role:{" "}
           <span className="pl-4 font-normal text-gray-800">
-            FrontEnd Developer
+            {singleJob?.title}
           </span>
         </h1>
         <h1 className="font-bold my-1">
           Location:{" "}
-          <span className="pl-4 font-normal text-gray-800">Hyderabad</span>
+          <span className="pl-4 font-normal text-gray-800">
+            {singleJob?.location}
+          </span>
         </h1>
         <h1 className="font-bold my-1">
           Description:{" "}
           <span className="pl-4 font-normal text-gray-800">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam
-            aliquid dolores totam optio molestiae, facere sunt quo rem
-            blanditiis.
+            {singleJob?.description}
           </span>
         </h1>
         <h1 className="font-bold my-1">
           Experience:{" "}
-          <span className="pl-4 font-normal text-gray-800">2 yrs</span>
+          <span className="pl-4 font-normal text-gray-800">
+            {singleJob?.experienceLevel}
+          </span>
         </h1>
         <h1 className="font-bold my-1">
-          Salary: <span className="pl-4 font-normal text-gray-800">12LPA</span>
+          Salary:{" "}
+          <span className="pl-4 font-normal text-gray-800">
+            {singleJob?.salary}LPA
+          </span>
         </h1>
         <h1 className="font-bold my-1">
           Total Applicants:{" "}
-          <span className="pl-4 font-normal text-gray-800">4</span>
+          <span className="pl-4 font-normal text-gray-800">
+            {singleJob?.applications?.length}
+          </span>
         </h1>
         <h1 className="font-bold my-1">
           Posted Date:{" "}
