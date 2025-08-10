433e0c992dbdc4c553b7b0ff779d18bd318ac283
arnav1203 <2101641530040@psit.ac.in>
2025-08-10 12:51:41 +0530

    Mail and contact

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -2,31 +2,39 @@ import React from "react";
 import Navbar from "./shared/Navbar";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
-import { Pen } from "lucide-react";
+import { Contact, Mail, Pen } from "lucide-react";
 
 const Profile = () => {
   return (
     <div>
       <Navbar />
       <div className="max-w-4xl mx-auto bg-white border border-gray-200 rounded-2xl my-5 p-8">
-        <div className="flex justify-between"></div>
-        <div className="flex items-center gap-4">
-          <Avatar className="h-24 w-24">
-            <AvatarImage
-              src="https://www.shutterstock.com/image-vector/circle-line-simple-design-logo-600nw-2174926871.jpg"
-              alt="profile"
-            />
-          </Avatar>
-          <div>
-            <h1 className="font-medium text-xl">Full Name</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-              rerum{" "}
-            </p>
+        <div className="flex justify-between">
+          <div className="flex items-center gap-4">
+            <Avatar className="h-24 w-24">
+              <AvatarImage
+                src="https://www.shutterstock.com/image-vector/circle-line-simple-design-logo-600nw-2174926871.jpg"
+                alt="profile"
+              />
+            </Avatar>
+            <div>
+              <h1 className="font-medium text-xl">Full Name</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+                rerum{" "}
+              </p>
+            </div>
           </div>
           <Button className="text-right" variant="outline">
             <Pen />
           </Button>
+        </div>
+        <div>
+          <Mail />
+          <span>arnav@gmail.com</span>
+
+          <Contact />
+          <span>0987654321</span>
         </div>
       </div>
     </div>
