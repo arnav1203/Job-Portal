fc2f0d006ba6d3d3bd3ba0a30cc55f8704e08645
arnav1203 <2101641530040@psit.ac.in>
2025-08-11 17:21:10 +0530

    Applicants table component

--- client/src/components/admin/Applicants.jsx
+++ client/src/components/admin/Applicants.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Navbar from "../shared/Navbar";
+import ApplicantsTable from "./ApplicantsTable";
 
 const Applicants = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <div className="max-w-7xl mx-auto">
+        <h1 className="font-bold text-xl my-5">
+          Applicants {applicants?.applications?.length}
+        </h1>
+        <ApplicantsTable />
+      </div>
+    </div>
+  );
 };
 
 export default Applicants;
