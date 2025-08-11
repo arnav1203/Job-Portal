4188eefafc50cadbedd449206e94bd91be716f09
arnav1203 <2101641530040@psit.ac.in>
2025-08-11 18:34:07 +0530

    showing applied jobs with status

--- client/src/components/AppliedJobTable.jsx
+++ client/src/components/AppliedJobTable.jsx
@@ -9,8 +9,10 @@ import {
   TableRow,
 } from "./ui/table";
 import { Badge } from "./ui/badge";
+import { useSelector } from "react-redux";
 
 const AppliedJobTable = () => {
+  const { allAppliedJobs } = useSelector((store) => store.job);
   return (
     <div>
       <Table>
@@ -27,27 +29,31 @@ const AppliedJobTable = () => {
           {/* {allAppliedJobs.length <= 0 ? (
             <span>You haven't applied any job yet.</span>
           ) : ( */}
-          {[1, 2, 3, 4].map((item, index) => (
-            <TableRow key={index}>
-              <TableCell>17-07-25</TableCell>
-              <TableCell>Frontend Developer</TableCell>
-              <TableCell>Google</TableCell>
-              <TableCell className="text-right">
-                <Badge
-                // className={`${
-                //   appliedJob?.status === "rejected"
-                //     ? "bg-red-400"
-                //     : appliedJob.status === "pending"
-                //     ? "bg-gray-400"
-                //     : "bg-green-400"
-                // }`}
-                >
-                  {/* {appliedJob.status.toUpperCase()} */}
-                  Selected
-                </Badge>
-              </TableCell>
-            </TableRow>
-          ))}
+          {allAppliedJobs.length <= 0 ? (
+            <span>You haven't applied any job yet.</span>
+          ) : (
+            allAppliedJobs.map((appliedJob) => (
+              <TableRow key={appliedJob._id}>
+                <TableCell>{appliedJob?.createdAt?.split("T")[0]}</TableCell>
+                <TableCell>{appliedJob?.job?.title}</TableCell>
+                <TableCell>{appliedJob.job.company}</TableCell>
+                <TableCell className="text-right">
+                  <Badge
+                    className={`${
+                      appliedJob?.status === "rejected"
+                        ? "bg-red-400"
+                        : appliedJob.status === "pending"
+                        ? "bg-gray-400"
+                        : "bg-green-400"
+                    }`}
+                  >
+                    {appliedJob.status.toUpperCase()}
+                    Selected
+                  </Badge>
+                </TableCell>
+              </TableRow>
+            ))
+          )}
           {/* )} */}
         </TableBody>
       </Table>
