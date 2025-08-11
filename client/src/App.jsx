3fcd87220bd4f3b3d108fc94a540dda07cbb6fc5
arnav1203 <2101641530040@psit.ac.in>
2025-08-11 16:56:38 +0530

    Post Job page building

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import Companies from "./components/admin/Companies";
 import CompanyCreate from "./components/admin/CompanyCreate";
 import CompanySetup from "./components/admin/CompanySetup";
 import AdminJobs from "./components/admin/AdminJobs";
+import PostJob from "./components/admin/PostJob";
 const approuter = createBrowserRouter([
   {
     path: "/",
@@ -56,6 +57,10 @@ const approuter = createBrowserRouter([
   {
     path: "/admin/jobs",
     element: <AdminJobs />,
+  },
+  {
+    path: "/admin/jobs/create",
+    element: <PostJob />,
   },
 ]);
 function App() {
