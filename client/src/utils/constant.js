4f0c63b1f6d7941b1a4492c8a198e542c66cbbb8
arnav1203 <2101641530040@psit.ac.in>
2025-08-12 01:43:14 +0530

    different for both local and production

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,3 +1,4 @@
+//When running locally
 export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
 export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
 export const APPLICATION_API_END_POINT = "http://localhost:8000/api/v1/application";
@@ -5,7 +6,7 @@ export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
 
 
 
-
+//When deploying to production
 
 // const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 // console.log("API_BASE_URL:", API_BASE_URL);
