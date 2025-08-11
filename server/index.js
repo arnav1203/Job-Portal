8adf1494bfd0217432377241d6f33ae5bad5cb7f
arnav1203 <2101641530040@psit.ac.in>
2025-08-12 00:51:02 +0530

    cors updated

--- server/index.js
+++ server/index.js
@@ -16,10 +16,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
     credentials: true
-}
+};
 app.use(cors(corsOptions));
+
 
 const PORT = process.env.PORT || 5000;
 
